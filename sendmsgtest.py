b3e7858588155053cc28555e135708b1d50085c6
jackyk <jackykimani13@gmail.com>
2016-05-03 10:04:27 +0300

    Test1 mondaymorning

---
+++ sendmsgtest.py
@@ -0,0 +1,9 @@
+from AfricasTalkingGateway import  AfricasTalkingGateway, AfricasTalkingGatewayException
+
+username="JACKYK"
+key= "695a771b995a729dfc94f5e563b2adeb786ba67c719af9f480f6b756c590a2a4"
+phoneNumber_=+254701383210
+
+mymsg= AfricasTalkingGateway(username,key)
+
+mymsg=(+254701383210 , "Test if it working")
